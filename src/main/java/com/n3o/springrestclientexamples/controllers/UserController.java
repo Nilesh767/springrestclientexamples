20b59f8d4db193f170d32fc0161a076754307dc3
nilesh767 <preneon@767>
2020-06-25 15:38:37 +0530

    Used Spring Web Client for reactive apis.#1 not fixed yet either.

--- src/main/java/com/n3o/springrestclientexamples/controllers/UserController.java
+++ src/main/java/com/n3o/springrestclientexamples/controllers/UserController.java
@@ -5,7 +5,6 @@ import com.n3o.springrestclientexamples.services.ApiService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.server.ServerWebExchange;
@@ -27,7 +26,9 @@ public class UserController {
 
     @PostMapping("/users")
     public String formPost(Model model, ServerWebExchange serverWebExchange){
-        MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
+        //Was for Spring Web MVC
+
+/*      MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
         Integer limit = new Integer(map.get("limit").get(0));
         log.debug("Received limit value: "+ limit);
         //default if null or zero
@@ -36,6 +37,14 @@ public class UserController {
             limit =1;
         }
         model.addAttribute("users",apiService.getUsers(limit));
+*/
+        model.addAttribute("users",
+                apiService
+                        .getUsers(serverWebExchange
+                                    .getFormData()
+                                    .map(data -> new Integer(data.getFirst("limit")))
+                        )
+        );
         return "userlist";
     }
 }
